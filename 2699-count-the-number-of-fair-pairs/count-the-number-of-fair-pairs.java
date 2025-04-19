f9bdbfa0fc98ca465a7610eaf2c5751ed3403081
tanishkavrshney <183093446+tanishkavrshney@users.noreply.github.com>
2025-04-19 20:38:48 +0530

    Time: 65 ms (34.08%) | Memory: 57.6 MB (21.34%) - LeetSync

---
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        long count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int left = lowerBound(nums, i + 1, nums.length - 1, lower - nums[i]);
+            int right = upperBound(nums, i + 1, nums.length - 1, upper - nums[i]);
+            count += (right - left + 1);
+        }
+
+        return count;
+    }
+
+    private int lowerBound(int[] nums, int start, int end, int target) {
+        int l = start, r = end, ans = end + 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] >= target) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    private int upperBound(int[] nums, int start, int end, int target) {
+        int l = start, r = end, ans = start - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] <= target) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
